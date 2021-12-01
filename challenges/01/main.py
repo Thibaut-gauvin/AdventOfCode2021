f72f99f8ba597b049e87b5b1f1bb56897330ce7a
Thibaut Gauvin <thibaut.gauvin@radiofrance.com>
2021-12-01 23:31:24 +0100

    Challenge 01 ⭐⭐

--- challenges/01/main.py
+++ challenges/01/main.py
@@ -1,4 +1,4 @@
-def main():
+def part1():
     counter = 0
     previous_value = None
 
@@ -15,6 +15,29 @@ def main():
         return counter
 
 
+def part2():
+    counter = 0
+    seq = []
+    window_size = 3
+    previous_value = None
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            intval = int(line.strip())
+            seq.append(intval)
+
+        for i in range(len(seq) - window_size + 1):
+            current = seq[i: i + window_size]
+            if previous_value is None:
+                previous_value = current
+                continue
+            if sum(current) > sum(previous_value):
+                counter += 1
+            previous_value = current
+
+        return counter
+
+
 if __name__ == "__main__":
-    result = main()
+    result = part2()
     print(result)
