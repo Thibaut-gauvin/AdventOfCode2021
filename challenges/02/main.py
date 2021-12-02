02c0b0bca39cf33bbd132b0bdc7a8bd5c3468b8d
Thibaut Gauvin <thibaut.gauvin@radiofrance.com>
2021-12-02 08:59:38 +0100

    Challenge 02 ⭐⭐

--- challenges/02/main.py
+++ challenges/02/main.py
@@ -19,6 +19,28 @@ def part1():
         return coordinates["horizon"] * coordinates["deeps"]
 
 
+def part2():
+    coordinates = {"horizon": 0, "deeps": 0, "aim": 0}
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            stripped = line.strip()
+            current = stripped.split()
+            action_key = current[0]
+            action_value = int(current[1])
+
+            if action_key == "forward":
+                coordinates["horizon"] += action_value
+                coordinates["deeps"] += coordinates["aim"] * action_value
+            if action_key == "up":
+                coordinates["aim"] -= action_value
+            if action_key == "down":
+                coordinates["aim"] += action_value
+
+        print(coordinates)
+        return coordinates["horizon"] * coordinates["deeps"]
+
+
 if __name__ == "__main__":
-    result = part1()
+    result = part2()
     print(result)
