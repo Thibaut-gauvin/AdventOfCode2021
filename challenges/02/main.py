1e1946c425300204bdc6e4381f79f6a8a574c724
Thibaut Gauvin <thibaut.gauvin@radiofrance.com>
2021-12-02 08:53:06 +0100

    Challenge 02 ⭐

---
+++ challenges/02/main.py
@@ -0,0 +1,24 @@
+def part1():
+    coordinates = {"horizon": 0, "deeps": 0}
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            stripped = line.strip()
+            current = stripped.split()
+            action_key = current[0]
+            action_value = int(current[1])
+
+            if action_key == "forward":
+                coordinates["horizon"] += action_value
+            if action_key == "up":
+                coordinates["deeps"] -= action_value
+            if action_key == "down":
+                coordinates["deeps"] += action_value
+
+        print(coordinates)
+        return coordinates["horizon"] * coordinates["deeps"]
+
+
+if __name__ == "__main__":
+    result = part1()
+    print(result)
